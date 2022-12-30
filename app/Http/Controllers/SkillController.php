9724808c09ff928d7506048c3839384054e626e9
DESKTOP-QUL61PF\Uriel David <uriel.azevedo.ext@spms.min-saude.pt>
2022-12-30 17:55:19 +0000

    adjusts

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -59,17 +59,6 @@ class SkillController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
