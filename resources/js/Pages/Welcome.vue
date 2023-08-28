ce481fd21d8fc152cbbc94ab8c9ef83d5023444c
Uriel-David <uriel.dqaa@gmail.com>
2023-08-28 16:45:11 +0100

    refactor: the description meta tag content 2

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,12 +23,12 @@ defineProps({
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.uriel-portfolio.com/" />
         <meta property="og:title" content="Uriel David - Software Solutions" />
-        <meta property="og:description" content="Website for exposing my portfolio and contracting my services." />
+        <meta property="og:description" content="Site for displaying my portfolio and work for others. On this site you will be able to see my work and skills in more detail, and if you wish, you can hire my services through a contact form or by email." />
         <meta property="og:image" content="https://github.com/Uriel-David.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.uriel-portfolio.com/" />
         <meta property="twitter:title" content="Uriel David - Software Solutions" />
-        <meta property="twitter:description" content="Website for exposing my portfolio and contracting my services." />
+        <meta property="twitter:description" content="Site for displaying my portfolio and work for others. On this site you will be able to see my work and skills in more detail, and if you wish, you can hire my services through a contact form or by email." />
         <meta property="twitter:image" content="https://github.com/Uriel-David.png" />
     </Head>
     <FrontendLayout>
